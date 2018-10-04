aa5d2a1be5b7146798014488121fee2885d40d0c
gauswamimilan <gauswamimilan248@gmail.com>
2018-10-04 11:56:14 +0530

    "adding instalation for nginx"

---
+++ installation/extra.sh
@@ -0,0 +1,8 @@
+apt-get install nginx -y > /dev/null
+/etc/init.d/nginx start
+rm /etc/nginx/sites-enabled/default
+touch /etc/nginx/sites-available/flask_settings
+ln -s /etc/nginx/sites-available/flask_settings /etc/nginx/sites-enabled/flask_settings
+cat server_setting.txt >> /etc/nginx/sites-enabled/flask_settings
+/etc/init.d/nginx restart > /dev/null
+gunicorn app:app
