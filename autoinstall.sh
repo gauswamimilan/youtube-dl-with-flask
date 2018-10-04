fc198fa150b3e33007021ed015ef4a89c7826ae6
gauswamimilan <gauswamimilan248@gmail.com>
2018-10-04 13:04:45 +0530

    "changing auto install"

--- autoinstall.sh
+++ autoinstall.sh
@@ -8,9 +8,13 @@ echo "-sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 echo "-sudo chmod a+rx /usr/local/bin/youtube-dl"
 sudo chmod a+rx /usr/local/bin/youtube-dl
-echo "-pip3 install virtualenv -y > /dev/null"
+echo "-pip3 install virtualenv > /dev/null"
 pip3 install virtualenv > /dev/null
-echo "-virtualenv ../venv > /dev/null > /dev/null"
+echo "-pip3 install flask > /dev/null"
+pip3 install flask > /dev/null
+echo "-pip3 install gunicorn > /dev/null"
+pip3 install gunicorn > /dev/null
+echo "-virtualenv venv > /dev/null"
 virtualenv venv > /dev/null
 echo "-apt-get install python -y > /dev/null"
 apt-get install python -y > /dev/null
