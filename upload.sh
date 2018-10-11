61555d16f6a4f3ea002c12d2b44b1abcc4311d7f
gauswamimilan <gauswamimilan248@gmail.com>
2018-10-11 12:57:58 +0530

    updating script

--- upload.sh
+++ upload.sh
@@ -6,11 +6,11 @@ git commit -a -m "$name"
 #git remote add origin https://github.com/gauswamimilan/youtube-dl-with-flask
 echo "enter branch 1:-master 2:-dirtest"
 read branch
-if [ $1 == 1 ]
+if [ $branch == 1 ]
 then
 	git push -u origin master
 fi
-if [ $1 == 2 ]
+if [ $branch == 2 ]
 then
 	git push -u origin dirtest
 fi
