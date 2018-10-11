a671e2c9059d56dc7ef4195f13173016992d139f
gauswamimilan <gauswamimilan248@gmail.com>
2018-10-11 12:26:16 +0530

    testing new branch

--- upload.sh
+++ upload.sh
@@ -3,5 +3,5 @@ git add .
 echo "enter commit name"
 read name
 git commit -a -m "$name"
-gir remote add origin https://github.com/gauswamimilan/youtube-dl-with-flask
+#git remote add origin https://github.com/gauswamimilan/youtube-dl-with-flask
 git push -u origin dirtest
