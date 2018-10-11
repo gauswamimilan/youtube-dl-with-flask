d8e1870a24913b7b8ea3a9c3467385344034817d
gauswamimilan <gauswamimilan248@gmail.com>
2018-10-11 12:59:31 +0530

    autoscript change

--- upload.sh
+++ upload.sh
@@ -6,11 +6,4 @@ git commit -a -m "$name"
 #git remote add origin https://github.com/gauswamimilan/youtube-dl-with-flask
 echo "enter branch 1:-master 2:-dirtest"
 read branch
-if [ $branch == 1 ]
-then
-	git push -u origin master
-fi
-if [ $branch == 2 ]
-then
-	git push -u origin dirtest
-fi
+git push -u origin $branch
